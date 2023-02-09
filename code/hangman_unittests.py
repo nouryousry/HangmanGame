0fe3656605afed210694513feb37b97f1c33db8e
Nourhan Omar <nourhan149751@bue.edu.eg>
2023-02-09 22:37:06 +0100

    unittests
    some cleanup ton the main code and adding unit tests

---
+++ code/hangman_unittests.py
@@ -0,0 +1,30 @@
+import unittest
+import random
+import hangman
+
+
+class HangmanTests(unittest.TestCase):
+
+    def test_display_hangman(self):
+        tries = 4
+        result = hangman.display_hangman(tries)
+        self.assertEqual( """
+               --------
+               |      |
+               |      
+               |    
+               |      
+               |     
+               -
+            """, result)
+    def test_word_bank(self):
+        self.assertIn(hangman.pick_word("easy"),hangman.easy_wordbank)
+
+    def test_play_game(self):
+        random.seed(0)
+        result = hangman.play_game()
+        self.assertIsNone(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
